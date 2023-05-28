a7ca51546cc618139d3f52b4673028979a467b60
svgarik <71320932+svgarik@users.noreply.github.com>
2023-05-29 00:30:20 +0300

    Перереализация двухэтапных эффектов + Ульта Пешки

--- spell.py
+++ spell.py
@@ -1,5 +1,5 @@
 import typing as tp
-from random import random
+from random import random, randrange
 from effect import *
 
 if tp.TYPE_CHECKING:
@@ -31,14 +31,61 @@ class Spell:
         self.cooldown_now = 0
         self.cost = cost
 
-        def zone(spell, self: "Piece", host_cell: "Square" = None):
-            pass
+    def zone(spell, self: "Piece", host_cell: "Square" = None) -> list[tuple[int, int]]:
 
-        def target(spell, self: "Piece"):
-            pass
+        """
+        Воспомогательная функция, возвращающая набор координат потенциальных клеток.
+        Требуется отдельная фильтрация и перевод в объекты класса Square
+        """
+
+        pass
+
+    def target(spell, self: "Piece") -> list["Square"]:
+
+        """
+        Фильтрует клетки из Zone оставляя только те, которые могут быть целевыми
+        """
         
-        def cast(spell, self: "Piece", other: "Square"):
-            pass
+        pass
+        
+    def cast(spell, self: "Piece", other: "Square") -> tp.Union["Spell", None]:
+
+        """
+        Производит действие способности и вернёт способность, которая должна заменить данную на интерфейсе
+        """
+
+        pass
+
+    def draw_rhomb(self, r_start, r_end, x0, y0) -> list[tuple[int, int]]:
+        """
+        Одно из правил выделения зоны, что встречается часто
+        """
+
+        #Хранилище
+        rhomb = []
+
+        #Перебераем радиусы
+        for r in range(r_start, r_end + 1):
+            x = x0
+            y = y0 + r
+
+            #выполняем обход по "ромбу" сетки основа которого - крест с дальностью r
+            for i in range(4 * r):
+                if i < r:
+                    x += 1
+                elif i < 3 * r:
+                    x -= 1
+                else:
+                    x += 1
+
+                if i < 2 * r:
+                    y -= 1
+                else:
+                    y += 1
+
+                rhomb.append((x, y))
+
+        return rhomb
 
 class Piece_Move(Spell):
 
@@ -136,32 +183,15 @@ class PawnAttack1(Spell):
 
     def zone(spell, self: "Piece", host_cell: "Square" = None):
 
-        """
-        Воспомогательная функция, возвращающая набор координат потенциальных клеток.
-        Требуется отдельная фильтрация и перевод в объекты класса Square
-        """
-
+        #Берём исходныю клетку, если не сказано иначе
         if host_cell is None:
             host_cell = self.cell
 
-        row_pos, col_pos = host_cell.get_pos()
-        x, y = row_pos, col_pos
-            
-        #задаём список возможных целей для атаки
-        #пешка атакует на одну клетку вокруг себя
-        potential = [
-            (x - 1, y - 1),
-            (x - 1, y),
-            (x - 1, y + 1),
-            (x + 1, y - 1),
-            (x + 1, y + 1),
-            (x + 1, y),
-            (x, y - 1),
-            (x, y + 1)
-        ]
+        y0, x0 = host_cell.get_pos()
+        potential = spell.draw_rhomb(1, 1, x0, y0)
 
         return potential
-
+    
     def target(spell, self: "Piece", host_cell: "Square" = None):
 
         #Берём исходныю клетку, если не сказано иначе
@@ -172,11 +202,12 @@ class PawnAttack1(Spell):
 
         target_list = []
             
-        #cреди этих клеток можно атаковать только те, на которых стоят фигуры (система свой-чужой не работает хе)
-        for cell in potential:
-            piece = self.field.get_square_by_pos(cell[0], cell[1]).inner_piece
-            if self.field.is_into_map(cell[0], cell[1]) and not piece is None and piece.team != self.team:
-                 target_list.append(self.field.get_square_by_pos(cell[0], cell[1]))
+        #cреди этих клеток можно атаковать только противников
+        for cell_coor in potential:
+            x, y = cell_coor[0], cell_coor[1]
+            cell = self.field.get_square_by_pos(y, x)
+            if not cell is None and not cell.inner_piece is None and cell.inner_piece.team != self.team:
+                 target_list.append(cell)
 
         return target_list
         
@@ -187,8 +218,9 @@ class PawnAttack1(Spell):
 
         #Если фигура попала, она наносит урон равный своим хп
         if random() < self.accuracy:
-            print(f"Атакующая фигура попала и нанесла {self.damage} урона!")
-            other.hp -= self.damage
+            damage = randrange(self.min_damage, self.max_damage + 1)
+            print(f"Атакующая фигура попала и нанесла {damage} урона!")
+            other.hp -= damage
             print(f"Оставшиеся хп жертвы: {other.hp}/{other.max_hp}")
 
             if [effect.id for effect in other.effect_list].count('speed_reduction') == 0:
@@ -210,41 +242,13 @@ class PawnAttack2_Move(Spell):
         super().__init__("lunge_move", "Выпад", "подойдите к противнику, чтобы потом атаковать (ближний бой)", 2, 2)
 
     def zone(spell, self: "Piece", host_cell: "Square" = None):
-        #Дальность выпада
-        lunge_range = 1 
-
-        #Хранилище
-        potential = []
-
+        
         #Берём исходныю клетку, если не сказано иначе
         if host_cell is None:
             host_cell = self.cell
 
-        #Переходим в систему координат
-        row_pos, col_pos = host_cell.get_pos()
-        y1, x1 = row_pos, col_pos
-
-
-        #Перебераем радиусы
-        for r in range(1, lunge_range + 1):
-            x = x1
-            y = y1 + r
-
-            #выполняем обход по "ромбу" сетки основа которого - крест с дальностью r
-            for i in range(4 * r):
-                if i < r:
-                    x += 1
-                elif i < 3 * r:
-                    x -= 1
-                else:
-                    x += 1
-
-                if i < 2 * r:
-                    y -= 1
-                else:
-                    y += 1
-
-                potential.append((x, y))
+        y0, x0 = host_cell.get_pos()
+        potential = spell.draw_rhomb(1, 1, x0, y0)
 
         return potential
     
@@ -271,8 +275,7 @@ class PawnAttack2_Move(Spell):
     def cast(spell, self: "Piece", other: "Square"):
         
         self.spell_list[0].cast(self, other)
-
-        return PawnAttack2_Attack()
+        self.spell_list[self.spell_list.index(spell)] = PawnAttack2_Attack()
 
 class PawnAttack2_Attack(Spell):
 
@@ -283,37 +286,12 @@ class PawnAttack2_Attack(Spell):
         #Дальность выпада
         lunge_range = 1 
 
-        #Хранилище
-        potential = []
-
         #Берём исходныю клетку, если не сказано иначе
         if host_cell is None:
             host_cell = self.cell
 
-        #Переходим в систему координат
-        row_pos, col_pos = host_cell.get_pos()
-        y1, x1 = row_pos, col_pos
-
-        #Перебераем радиусы
-        for r in range(1, lunge_range + 1):
-            x = x1
-            y = y1 + r
-
-            #выполняем обход по "ромбу" сетки основа которого - крест с дальностью r
-            for i in range(4 * r):
-                if i < r:
-                    x += 1
-                elif i < 3 * r:
-                    x -= 1
-                else:
-                    x += 1
-
-                if i < 2 * r:
-                    y -= 1
-                else:
-                    y += 1
-
-                potential.append((x, y))
+        y0, x0 = host_cell.get_pos()
+        potential = spell.draw_rhomb(1, 1, x0, y0)
 
         return potential
     
@@ -339,10 +317,63 @@ class PawnAttack2_Attack(Spell):
     def cast(spell, self: "Piece", other: "Square"):
         
         PawnAttack1().cast(self, other)
+        self.spell_list[self.spell_list.index(spell)] = PawnAttack2_Move()
+
 
 class PawnUtility(Spell):
 
     def __init__(self):
         super().__init__("just_pawn", "Всего лишь пешка", "Фигура делает сильную атаку и  наносит себе 50% от нанесенного урона (ближний бой)", 3, 2)
 
-    pass
+    def zone(spell, self: "Piece", host_cell: "Square" = None):
+
+        #Берём исходныю клетку, если не сказано иначе
+        if host_cell is None:
+            host_cell = self.cell
+
+        y0, x0 = host_cell.get_pos()
+        potential = spell.draw_rhomb(1, 1, x0, y0)
+
+        return potential
+    
+    def target(spell, self: "Piece", host_cell: "Square" = None):
+
+        #Берём исходныю клетку, если не сказано иначе
+        if host_cell is None:
+            host_cell = self.cell
+        
+        potential = spell.zone(self, host_cell = host_cell)
+
+        target_list = []
+            
+        #cреди этих клеток можно атаковать только противников
+        for cell_coor in potential:
+            x, y = cell_coor[0], cell_coor[1]
+            cell = self.field.get_square_by_pos(y, x)
+            if not cell is None and not cell.inner_piece is None and cell.inner_piece.team != self.team:
+                 target_list.append(cell)
+
+        return target_list
+    
+    def cast(spell, self: "Piece", other: "Square"):
+        
+        #забираем фигуру из клетки
+        other = other.inner_piece
+
+        #Фигура попала и она наносит урон*1.5
+        damage = randrange(self.min_damage, self.max_damage + 1)
+        damage *= 2
+        print(f"Атакующая фигура попала и нанесла {damage} урона!")
+        other.hp -= damage
+        self.hp -= damage // 2
+        print(f"Оставшиеся хп жертвы: {other.hp}/{other.max_hp}")
+        print(f"Оставшиеся хп свои: {self.hp}/{self.max_hp}")
+
+        #Если фигуры хп падают до 0 и ниже, удаляем её с поля
+        if other.hp <= 0:
+            print("Сильный удар разбивает жертву в каменную крошку!")
+            other.cell.del_inner_piece()
+
+        if self.hp <= 0:
+            print("Эта жертва не будет напрасной...")
+            self.cell.del_inner_piece()
